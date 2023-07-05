20fdb428ede84e7a1194bbdaf0b84d84ca53c2dc
Nykuluz <carlmillanes3@gmail.com>
2023-07-05 19:00:13 +0800

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Carl Irven Nykuluz F. Millanes on 7/5/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
